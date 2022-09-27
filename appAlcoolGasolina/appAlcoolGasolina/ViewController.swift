f6e23c881d4e9111be7853d656ac6308e84cb3c9
KarinaMonteiro <karina.kk93@gmail.com>
2022-09-26 23:07:41 -0300

    habilitando/desabilitando botao

--- appAlcoolGasolina/appAlcoolGasolina/ViewController.swift
+++ appAlcoolGasolina/appAlcoolGasolina/ViewController.swift
@@ -13,21 +13,30 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var precoalcoolTextField: UITextField!
     
     @IBOutlet weak var precoGasolinaTextField: UITextField!
-    	
+    
+    @IBOutlet weak var calcularButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configTextField()
+        configButton()
     }
     
     func configTextField(){
         precoalcoolTextField.delegate = self
         precoGasolinaTextField.delegate = self
     }
+    
+    func configButton(){
+        calcularButton.isEnabled = false
+    }
+    
     func textFieldDidBeginEditing(_ textField: UITextField) {
         textField.layer.borderColor = UIColor.blue.cgColor
         textField.layer.borderWidth = 2
         
     }
+    
     func textFieldDidEndEditing(_ textField: UITextField) {
         if textField.hasText{
             textField.layer.borderColor = UIColor.lightGray.cgColor
@@ -36,11 +45,16 @@ class ViewController: UIViewController, UITextFieldDelegate {
         }else{
             textField.layer.borderColor = UIColor.red.cgColor
             textField.layer.borderWidth = 2
-            
         }
-            
-
+        
+        if precoalcoolTextField.hasText && precoGasolinaTextField.hasText{
+            calcularButton.isEnabled = true
+        }
     }
-    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
 }
 
