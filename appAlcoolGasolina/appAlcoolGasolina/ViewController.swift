db674daf739dccad2f4933175d70d616f4a9b9f2
KarinaMonteiro <karina.kk93@gmail.com>
2022-09-26 21:25:32 -0300

    criando projeto

---
+++ appAlcoolGasolina/appAlcoolGasolina/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appAlcoolGasolina
+//
+//  Created by Tiago & Karina on 26/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
