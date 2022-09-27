636b0c201a844b50d73c2b708a51a22b8716f298
KarinaMonteiro <karina.kk93@gmail.com>
2022-09-26 22:47:48 -0300

    adicionando validacao de campo

--- appAlcoolGasolina/appAlcoolGasolina/ViewController.swift
+++ appAlcoolGasolina/appAlcoolGasolina/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var precoalcoolTextField: UITextField!
+    
+    @IBOutlet weak var precoGasolinaTextField: UITextField!
+    	
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configTextField()
     }
+    
+    func configTextField(){
+        precoalcoolTextField.delegate = self
+        precoGasolinaTextField.delegate = self
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.layer.borderColor = UIColor.blue.cgColor
+        textField.layer.borderWidth = 2
+        
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField.hasText{
+            textField.layer.borderColor = UIColor.lightGray.cgColor
+            textField.layer.borderWidth = 2
+            
+        }else{
+            textField.layer.borderColor = UIColor.red.cgColor
+            textField.layer.borderWidth = 2
+            
+        }
+            
 
-
+    }
+    
 }
 
